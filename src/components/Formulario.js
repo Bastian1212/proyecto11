bc33305ebfd3f239981b4be09d42abe350511177
Bastian1212 <bastianvillanueva@me.com>
2022-08-12 15:34:26 -0400

    iniciando  Formulario

---
+++ src/components/Formulario.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Formulario = () => {
+    return (  
+        <form className="col-12">
+            <fieldset className="text-center">
+                <legend>Busca bebidas por categoria o Ingrediente</legend>
+            </fieldset>
+            <div className="row mt-4">
+                <div className="col-md-4">
+                    <input
+                        name="nombre"
+                        className="form-control"
+                        type="text"
+                        placeholder="Buscar por Ingrediente"
+                    />
+                </div>
+                <div className="col-md-4">
+                    <select
+                        className="form-control"
+                        name="categoria"
+                    >
+                        <option value="">-- Selecciona Categoria</option>
+                    </select>
+                </div>
+                <div className="col-md-4">
+                    <input
+                        type="submit"
+                        className="btn btn-block btn-primary"
+                        value="Buscar Recetas"
+                    />
+                </div>
+            </div>
+        </form>
+    );
+}
+ 
+export default Formulario;
